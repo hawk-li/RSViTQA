3bd6317be20f01751218031fe7a3aa629a9edaaf
hawk-li <accounts@hawk.li>
2023-11-08 23:56:09 +0100

    attention v2

--- VQA_model/models/attention.py
+++ VQA_model/models/attention.py
@@ -1,17 +1,21 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.weight_norm import weight_norm
 
 class SelfAttention(nn.Module):
     def __init__(self, feature_size):
         super(SelfAttention, self).__init__()
         # Initialize weight matrices for Q, K, and V
-        self.W_q = nn.Linear(feature_size, feature_size, bias=False)
-        self.W_k = nn.Linear(feature_size, feature_size, bias=False)
-        self.W_v = nn.Linear(feature_size, feature_size, bias=False)
+        self.W_q = weight_norm(nn.Linear(feature_size, feature_size, bias=False))
+        self.W_k = weight_norm(nn.Linear(feature_size, feature_size, bias=False))
+        self.W_v = weight_norm(nn.Linear(feature_size, feature_size, bias=False))
         
         # Save the feature size to scale the dot product attention scores
         self.d_k = feature_size
+
+        # Initialize dropout layer
+        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
         # x is assumed to be of shape [batch_size, seq_len, feature_size]
@@ -30,6 +34,9 @@ class SelfAttention(nn.Module):
         # Apply softmax on the last dimension to obtain the attention probabilities
         attention_weights = F.softmax(scaled_attention_scores, dim=-1)
 
+        # Apply dropout on the attention weights
+        attention_weights = self.dropout(attention_weights)
+
         # Weight the values by the attention weights
         weighted_values = torch.matmul(attention_weights, V)
 
@@ -39,12 +46,15 @@ class CrossAttention(nn.Module):
     def __init__(self, feature_size):
         super(CrossAttention, self).__init__()
         # Initialize weight matrices for Q, K, and V
-        self.W_q = nn.Linear(feature_size, feature_size, bias=False)
-        self.W_k = nn.Linear(feature_size, feature_size, bias=False)
-        self.W_v = nn.Linear(feature_size, feature_size, bias=False)
+        self.W_q = weight_norm(nn.Linear(feature_size, feature_size, bias=False))
+        self.W_k = weight_norm(nn.Linear(feature_size, feature_size, bias=False))
+        self.W_v = weight_norm(nn.Linear(feature_size, feature_size, bias=False))
 
         # Save the feature size to scale the dot product attention scores
         self.d_k = feature_size
+
+        # Initialize dropout layer
+        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x, y):
         # x is assumed to be of shape [batch_size, seq_len_x, feature_size]
@@ -63,6 +73,9 @@ class CrossAttention(nn.Module):
 
         # Apply softmax on the last dimension to obtain the attention probabilities
         attention_weights = F.softmax(scaled_attention_scores, dim=-1)
+
+        # Apply dropout on the attention weights
+        attention_weights = self.dropout(attention_weights)
 
         # Weight the values by the attention weights
         weighted_values = torch.matmul(attention_weights, V)
